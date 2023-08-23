48a128bbfa86bd007a287ff7af216d5105caa5e3
manuelimal02 <manuelimal02@gmail.com>
2023-08-22 19:20:19 -0600

    Cambio Función Informe y Condicion de Venta

--- inventario_DAO.py
+++ inventario_DAO.py
@@ -31,25 +31,30 @@ class inventario_DAO:
             if cantidad < producto_existente.cantidad:
                 producto_existente.cantidad -= cantidad
                 print("El producto "+nombre +" ha sido vendido correctamente. Inventario Actualizado.")
-            elif cantidad >= producto_existente.cantidad:
-                print("ERROR: La cantidad a vender de "+nombre+" en la ubicación "+ubicacion+" es mayor o igual que la existencia.")
+            elif cantidad > producto_existente.cantidad:
+                print("ERROR: La cantidad a vender de "+nombre+" en la ubicación "+ubicacion+" es mayor.")
         else:
             print("ERROR: El producto "+nombre+" no existe en la ubicación "+ubicacion+".")
-
 
     def crear_informe(self, nombre_archivo):
         try:
             with open(nombre_archivo, "w") as archivo:
-                linea1="Informe de Inventario:" + "\n"+ "\n" 
-                archivo.write(linea1)
+                archivo.write("Informe de Inventario:" + "\n"+ "\n") 
+                archivo.write("{:<20} {:<15} {:<15}{:<15} {:<15}\n".format("Nombre", "Cantidad", "Precio","Valor Unitario", "Ubication"))
+                archivo.write("-" * 90 + "\n")
                 for producto in self.lista_productos:
                     valor_total=producto.cantidad*producto.precio
-                    linea = "Nombre: "+producto.nombre+", "+"Cantidad: "+str(producto.cantidad)+", "+"Precio: "+str(producto.precio)+", "+"Valor Total: "+str(valor_total)+", "+"Ubicacion: "+producto.ubicacion + "\n"
+
+                    nombre=producto.nombre.ljust(20)
+                    cantidad=str(producto.cantidad).ljust(20)
+                    precio=str(producto.precio).ljust(20)
+                    valor_t=str(valor_total).ljust(20)
+                    ubicacion=producto.ubicacion
+                    linea = "{:<20} {:<15} {:<15}{:<15} {:<15}\n".format(nombre,cantidad,precio,valor_t, ubicacion)
                     archivo.write(linea)
             print("Archivo",nombre_archivo," Creado Correctamente")
         except Exception as e:
             print("Error al crear el archivo:",e)
-
 
     def imprimir_productos(self):
         for producto in self.lista_productos:
